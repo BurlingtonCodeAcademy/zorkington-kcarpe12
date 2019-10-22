8c6f23d5e4e0b484feb509e13be7b41d1a05835f
KC <carpenter.katie.e@gmail.com>
2019-10-21 23:26:03 -0400

    the sloppy small bits of project I have so far...

--- index.js
+++ index.js
@@ -2,38 +2,76 @@ const readline = require('readline');
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
-  return new Promise((resolve, reject) => {
-    readlineInterface.question(questionText, resolve);
-  });
-}
-
-// remember the StateMachine lecture
-// https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
-let states = {
-  'roomOne': { canChangeTo: [ 'roomTwo' ] },
-  'roomTwo': { canChangeTo: [ 'roomThree' ] },
-  'roomThree': { canChangeTo: [ 'roomOne' ] }
-};
-
-let currentState = "green";
-
-function enterState(newState) {
-  let validTransitions = states[currentState].canChangeTo;
-  if (validTransitions.includes(newState)) {
-    currentState = newState;
-  } else {
-    throw 'Invalid state transition attempted - from ' + currentState + ' to ' + newState;
-  }
+    return new Promise((resolve, reject) => {
+        readlineInterface.question(questionText, resolve);
+    });
 }
 
 start();
 
 async function start() {
-  const welcomeMessage = `182 Main St.
+    const welcomeMessage = `182 Main St.
 You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
-  let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
-  process.exit();
+On the door is a handwritten sign.\n`;
+    let answer = await ask(welcomeMessage);
+    for (; answer;) {
+        if (answer.toLowerCase() === "open door") {
+            answer = await ask("The door is locked. There is a keypad on the door handle.\n");
+        }
+        else if (answer.toLowerCase() === "take sign")
+            answer = await ask("That would be selfish. How will other students find their way?\n");
+        else if (answer.toLowerCase() !== "read sign") {
+            answer = await ask('Sorry, I don\'t understand "' + answer + '."\n');
+        }
+        else {
+            sign();
+            break;
+        }
+    }
+}
+
+async function sign() {
+    let signText = 'The sign says "Welcome to Burlington Code Academy! Come on up\nto the third floor. If the door is locked, use the code 12345."\n';
+    let answer = await ask(signText);
+    for (; answer;) {
+        if (answer.toLowerCase().includes("take") && answer.toLowerCase().includes("sign")) {
+            answer = await ask("That would be selfish. How will other students find their way?\n");
+        }
+        else if (answer.toLowerCase().includes("enter code ") && !answer.toLowerCase().includes("12345")) {
+            answer = await ask("Bzzzzt! The door is still locked.\n")
+        }
+        else if (answer.toLowerCase() !== "enter code 12345") {
+            answer = await ask('Sorry, I don\'t understand "' + answer + '."\n');
+        }
+        else {
+            console.log("Success! The door opens. \nYou enter the foyer and the door shuts behind you.");
+            enter()
+            break;
+        }
+    }
+
+}
+
+async function enter() {
+    let foyerDesc = "You are in a foyer. A copy of Seven Days lies in a corner.\n"
+    let answer = await ask(foyerDesc);
+    let inventory = []
+    for (; answer;) {
+        if (answer.toLowerCase().includes("take") || answer.toLowerCase().includes("pick up") && answer.toLowerCase().includes("seven days") || answer.toLowerCase().includes("newspaper")) {
+            answer = await ask("You pick up the paper and leaf through it.\n")
+            inventory.push("newspaper");
+        }
+        else if (answer.toLowerCase() === "inventory") {
+            answer = await ask("You are carrying: " + inventory + "\n");
+        }
+        else if (answer.toLowerCase().includes("read") && answer.toLowerCase().includes("paper")) {
+            answer = await ask("Looks like another special on weed.\n")
+        }
+        else {
+            break;
+        }
+    }
+
+
 }
